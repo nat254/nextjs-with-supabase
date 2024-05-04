b88fa5a6eedcbf9000f1ba9a256c214dc3427351
nat254 <natalie.ndetei@strathmore.edu>
2024-05-04 18:20:25 +0300

    refactor: install dependency

--- app/protected/page.tsx
+++ app/protected/page.tsx
@@ -8,8 +8,6 @@ import { redirect } from "next/navigation";
 export default async function ProtectedPage() {
   const supabase = createClient();
 
-  
-
   const {
     data: { user },
   } = await supabase.auth.getUser();
